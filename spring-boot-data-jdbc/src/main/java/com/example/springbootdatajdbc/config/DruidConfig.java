24217ea65e2f9159fa3ac4fcbebc7460f8dc47d0
Jiusen <66288052+Jiusen@users.noreply.github.com>
2020-10-14 11:15:04 +0800

    Update DruidConfig.java

--- spring-boot-data-jdbc/src/main/java/com/example/springbootdatajdbc/config/DruidConfig.java
+++ spring-boot-data-jdbc/src/main/java/com/example/springbootdatajdbc/config/DruidConfig.java
@@ -20,4 +20,34 @@ public class DruidConfig {
     public DataSource druid(){
         return new DruidDataSource();
     }
+    
+    
+    //配置Druid的监控
+    //1、配置一个管理后台的Servlet
+    @Bean
+    public ServletRegistrationBean startViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "123456");
+        initParams.put("allow", "");//默认就是允许所有访问
+        initParams.put("deny", "192.168.246.128");
+
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+    //2、配置一个web监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<>();
+
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 }
